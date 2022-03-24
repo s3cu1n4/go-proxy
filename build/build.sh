550da1b739c58b958a2fa90de4df80fded5ac9df
s3cu1n4 <s3cu1n4@gmail.com>
2022-03-24 11:18:12 +0800

    update readme

--- build/build.sh
+++ build/build.sh
@@ -6,19 +6,16 @@ project_name="${project_path##*/}"
 cd $project_path
 
 # macos server
-go build -a ../server/server.go 
+CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -a -o darwin_server ../server/server.go 
 
 # linux server
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -a -o linux_server ../server/server.go 
 
 # macos client
-go build -a  -o client ../client/main.go 
+CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -a -o darwin_client ../client/main.go 
 
 # windows client
-CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -a  -o client.exe ../client/main.go
-
-
-
+CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -a  -o windows_client.exe ../client/main.go
 
 echo "build successful"
 
