7c704444297cec44017a4e3e5813ef1cf615b9e9
s3cu1n4 <s3cu1n4@gmail.com>
2022-03-30 16:52:59 +0800

    update build script

--- build/build.sh
+++ build/build.sh
@@ -6,16 +6,16 @@ project_name="${project_path##*/}"
 cd $project_path
 
 # macos server
-CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -a -o darwin_server ../server/server.go 
+CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -a -o ../bin/darwin_server ../server/server.go 
 
 # linux server
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -a -o linux_server ../server/server.go 
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -a -o ../bin/linux_server ../server/server.go 
 
 # macos client
-CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -a -o darwin_client ../client/main.go 
+CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -a -o ../bin/darwin_client ../client/main.go 
 
 # windows client
-CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -a  -o windows_client.exe ../client/main.go
+CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -a  -o ../bin/windows_client.exe ../client/main.go
 
 echo "build successful"
 
